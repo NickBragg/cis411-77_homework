258ecf7eafd00ffebfde1b1cf42db44b30ab10c3
NickBragg <braggnickp@gmail.com>
2014-03-16 11:13:21 -0400

    Initial Project Commit
    Started the project. Got the Manage page to work.

---
+++ homework6/.gitignore/MvcPet/MvcPet/Models/Pet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Data.Entity;
+
+namespace MvcPet.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateRecieved { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    //public class Inventory
+    //{
+    //    public int ID { get; set; }
+    //    public String Picture { get; set; }
+    //    public string Name { get; set; }
+    //    public string Description { get; set; }
+    //}
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+    }
+}
